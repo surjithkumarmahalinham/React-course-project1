787940838d226f00ae6ddc3bf794ae69590df7d6
M Surjith Kumar <surjith.kumar@sifycorp.com>
2024-02-26 16:39:39 +0530

    react course learn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  function handleNameChange(){
+    const names = ["vellore","chennai","thirunelveli"];
+    const int = Math.floor(Math.random()*3);
+    return names[int];
+  }
+  return(
+    <div>
+      Welcome to {handleNameChange()}
+      <p>Thank you for coming</p>
     </div>
-  );
+  )
 }
 
 export default App;
