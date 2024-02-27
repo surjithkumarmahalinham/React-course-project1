417f4adee70da283f552820355a800926612997d
M Surjith Kumar <surjith.kumar@sifycorp.com>
2024-02-27 12:31:31 +0530

    click_event

--- src/Content.js
+++ src/Content.js
@@ -3,14 +3,23 @@ import React from "react"
 const Content = () => {
 
     function handleNameChange(){
-        const names = ["vellore","chennai","thirunelveli"];
-        const int = Math.floor(Math.random()*3);
-        return names[int];
-      }
+      const names = ["vellore","chennai","thirunelveli"];
+      const int = Math.floor(Math.random()*3);
+      return names[int];
+    }
+
+    const handleClick = (e) =>{
+      console.log(e);
+    }
+    const handleClick2 = (name) =>{
+      console.log(`Thanks for click this button ${name}`);
+    }
     
   return (
     <main>
-      <p>Welcome to {handleNameChange()}. Thank you for coming</p>
+      <p onDoubleClick={() => handleClick2("surjith")}>Welcome to {handleNameChange()}. Thank you for coming</p>
+      <button onClick={(e) => handleClick(e)}>Click here</button>
+      <button onClick={() => handleClick2('msk')}>Click with name</button>
     </main>
   )
 }
